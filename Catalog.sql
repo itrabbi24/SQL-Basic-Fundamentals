23ea332786fcd10ec86052f0d3d284c836fc0ef4
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2024-10-30 14:04:12 +0600

    update catalog

--- Catalog.sql
+++ Catalog.sql
@@ -17,3 +17,39 @@ SELECT * FROM sys.fulltext_catalogs
 -- List full-text languages
 SELECT * FROM sys.fulltext_languages
 
+
+
+-- Rebuild a full-text catalog
+-- Run the Transact-SQL statement ALTER FULLTEXT CATALOG ... REBUILD, or do the following things in SQL Server Management Studio (SSMS).
+
+-- In SSMS, in Object Explorer, expand the server, expand Databases, and then expand the database that contains the full-text catalog that you want to rebuild.
+
+-- Expand Storage, and then expand Full Text Catalogs.
+
+-- Right-click the name of the full-text catalog that you want to rebuild, and select Rebuild.
+
+-- To the question Do you want to delete the full-text catalog and rebuild it?, click OK.
+
+-- In the Rebuild Full-Text Catalog dialog box, click Close.
+
+-- Rebuild all full-text catalogs for a database
+-- In SSMS, in Object Explorer, expand the server, expand Databases, and then expand the database that contains the full-text catalogs that you want to rebuild.
+
+-- Expand Storage, and then right-click Full Text Catalogs.
+
+-- Select Rebuild All.
+
+-- To the question, Do you want to delete all full-text catalogs and rebuild them?, click OK.
+
+-- In the Rebuild All Full-Text Catalogs dialog box, click Close.
+
+-- Remove a full-text catalog from a database
+-- Run the Transact-SQL statement DROP FULLTEXT CATALOG, or do the following things in SQL Server Management Studio (SSMS).
+
+-- In SSMS, in Object Explorer, expand the server, expand Databases, and expand the database that contains the full-text catalog you want to remove.
+
+-- Expand Storage, and expand Full Text Catalogs.
+
+-- Right-click the full-text catalog that you want to remove, and then select Delete.
+
+-- In the Delete Objects dialog box, click OK.
